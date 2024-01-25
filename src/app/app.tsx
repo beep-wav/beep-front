cfa07ee391d8ee24e653e585bb83c3c082284767
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-01-25 21:32:54 +0100

    configured tailwind prettier

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
     <div>
-      <NxWelcome title="beep-front" />
+      <h1 className="bottom-1 bg-slate-300 text-4xl text-red-500">
+        Hello, world!
+      </h1>
     </div>
   );
 }
