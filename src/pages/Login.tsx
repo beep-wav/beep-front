eccdd84e1a79023d259b5dfd04278cb2da7cc246
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-01-28 16:56:14 +0100

    login & authentication design

---
+++ src/pages/Login.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import AuthButton from '../components/button/AuthButton';
+import AuthInput from '../components/input/AuthInput';
+
+export interface LoginProps {
+}
+
+export default function Login(props: LoginProps) {
+  return (
+    <div className='bg-cloud h-screen w-full bg-no-repeat bg-cover flex justify-center'>
+        <div className='flex flex-col gap-6 justify-center items-start'>
+            <h1 className='font-extrabold'>Beep</h1>
+            <h5>We are happy to see you !</h5>
+            <AuthInput label='EMAIL' placeholder='Email' />
+            <AuthInput label='PASSWORD' placeholder='Password' />
+            <Link className='text-purple-600 font-medium' to={''}>Forgot password ?</Link>
+            <AuthButton title='Log in' />
+            <div className='flex flex-row gap-1'>
+                <h5 className='font-medium'>Don't have an account ?</h5>
+                <Link className='text-purple-600 font-medium' to={''}>Sign up</Link>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+
